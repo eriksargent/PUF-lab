dbd99cf68ef56f0c3a3a3eb1a45714125b596a42
Erik Sargent <erik@lightningkite.com>
2017-04-03 17:50:56 -0600

    Got the UART 8 bit buffers working

--- sources/controller.v
+++ sources/controller.v
@@ -31,49 +31,51 @@ module controller(
     reg [63:0] tx_data;
     wire [7:0] tx_output;
     reg tx_valid = 0;
+    wire tx_active;
+    reg prev_tx_active = 0;
     wire tx_ready;
     reg [63:0] rx_data;
     wire [7:0] rx_input;
     wire rx_valid;
+    reg prev_rx_valid = 0;
     reg rx_ready = 1;
     
     reg [2:0]in_byte_count = 0;
     reg [2:0]out_byte_count = 0;
     
     assign tx_output = tx_data[7:0];
-        
-    always @(posedge clk) begin
-        if (rst) begin
-            rst <= 0;
-            tx_valid <= 0;
-        end
-    end
-    
-    wire tx_active;
     
     uart_rx UR(clk, rx, rx_valid, rx_input);
     uart_tx UT(clk, tx_valid, tx_output, tx_active, tx, tx_ready); 
     
-    always @(posedge rx_valid) begin
-        rx_data <= (rx_data << 8) | rx_input;
-        in_byte_count <= in_byte_count + 1;
-        
-        if (in_byte_count == 7) begin
-            tx_data <= rx_data;
-            out_byte_count <= 7;
+    always @(posedge clk) begin
+        if (rst) begin
+            rst <= 0;
             tx_valid <= 1;
-            in_byte_count <= 0;
         end
-    end
-    
-    always @(posedge tx_ready) begin
-        tx_data <= tx_data >> 8;
-        out_byte_count <= out_byte_count - 1;
         
-        if (out_byte_count == 0) begin
-            tx_valid <= 0;
+        if (~prev_rx_valid && rx_valid) begin
+            rx_data <= (rx_data << 8) | rx_input;
+            in_byte_count <= in_byte_count + 1;
+            
+            if (in_byte_count == 7) begin
+                tx_data <= rx_data;
+                out_byte_count <= 7;
+                tx_valid <= 1;
+            end
         end
+        
+        prev_rx_valid <= rx_valid;
+
+        if (prev_tx_active && ~tx_active) begin
+            tx_data <= (tx_data >> 8) + 1;
+            out_byte_count <= out_byte_count - 1;
+            
+            if (out_byte_count == 0) begin
+                tx_valid <= 0;
+            end
+        end
+        
+        prev_tx_active <= tx_active;
     end
-    
-    
 endmodule
